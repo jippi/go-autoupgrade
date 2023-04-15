3cf396c374fbc4a2d11ffec587699df0d7176c9d
Christian Winther <jippignu@gmail.com>
2023-04-15 20:07:09 +0200

    refactor

--- update.sh
+++ update.sh
@@ -7,31 +7,37 @@ branch_name=$(date +%Y%m%d-%H%M)
 
 echo "=> Change branch to ${branch_name}"
 git co -b "${branch_name}"
+echo
 
 if [ ! -e releases.json ]
 then
     echo "=> Fetching Golang releases"
     curl -o releases.json "https://go.dev/dl/?mode=json&include=all"
+    echo
 fi
 
 # extract go runtimes
 echo "=> Generate ${filename}"
 jq -r --argjson config "$(cat config.json)" "$(cat update.jq)" releases.json | tee $filename
+echo
 
 echo "=> Check for updates"
 if git diff --quiet $filename
 then
     echo "no changes, good"
+    echo
     exit 1
 fi
 
 echo "=> Detected change!"
 git --no-pager diff $filename
+echo
 
 echo "=> Commit changes"
 git commit \
     -m "update go versions" \
     $filename
+echo
 
 # https://docs.gitlab.com/ee/user/project/push_options.html#push-options-for-merge-requests
 echo "=> Push changes"
@@ -43,3 +49,4 @@ git push \
     -o merge_request.merge_when_pipeline_succeeds \
     -o merge_request.remove_source_branch \
     -o merge_request.label=automatic-upgrade
+echo
