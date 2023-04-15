68465cb6b8fa5eed077d6a8c4096d6c65dae4aff
Christian Winther <jippignu@gmail.com>
2023-04-15 19:18:22 +0200

    refactor

--- update.sh
+++ update.sh
@@ -5,7 +5,5 @@ then
     curl -o releases.json "https://go.dev/dl/?mode=json&include=all"
 fi
 
-exec jq \
-    -r \
-    --argjson input "$(cat args.json)" \
-    "$(cat update.jq)" releases.json | tee runtimes.txt
+# extract go runtimes
+jq -r --argjson input "$(cat args.json)" "$(cat update.jq)" releases.json | tee runtimes.txt
