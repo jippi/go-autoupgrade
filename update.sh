fc92f229eed0057fd7e96531510842be2c328e6e
Christian Winther <jippignu@gmail.com>
2023-04-15 19:20:46 +0200

    refactor

--- update.sh
+++ update.sh
@@ -7,3 +7,18 @@ fi
 
 # extract go runtimes
 jq -r --argjson input "$(cat args.json)" "$(cat update.jq)" releases.json | tee runtimes.txt
+
+if ! git diff-index --quiet HEAD --
+then
+    echo "no changes, good"
+    exit 1
+fi
+
+echo "found changes"
+git commit \
+    -o merge_request.create \
+    -o merge_request.merge_when_pipeline_succeeds \
+    -o merge_request.remove_source_branch \
+    -o merge_request.label=automatic-upgrade \
+    -a \
+    -m "update go versions"
