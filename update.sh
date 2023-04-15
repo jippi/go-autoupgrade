922b33cde8b9a679b92462a3358d95365874dab8
Christian Winther <jippignu@gmail.com>
2023-04-15 19:28:23 +0200

    refactor

--- update.sh
+++ update.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+branch_name=$(date +%Y%m%d-%H%M)
+
+echo "=> Change branch to ${branch_name}"
+git co -b "${branch_name}"
+
 if [ ! -e releases.json ]
 then
+    echo "=> Fetching Golang releases"
     curl -o releases.json "https://go.dev/dl/?mode=json&include=all"
 fi
 
 # extract go runtimes
+echo "=> Generate runtimes.txt"
 jq -r --argjson input "$(cat args.json)" "$(cat update.jq)" releases.json | tee runtimes.txt
 
+echo "=> Check for updates"
 if git diff-index --quiet HEAD --
 then
     echo "no changes, good"
     exit 1
 fi
 
-echo "found changes"
+echo "=> Commit changes"
 git commit \
     -a \
     -m "update go versions"
 
+echo "=> Push changes"
 git push \
     -u origin \
     main:$(date +%Y%m%d-%H%M)
